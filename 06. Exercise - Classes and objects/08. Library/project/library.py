701df945688397f6dae785e08bc5f2dc23fdf7fa
abruski <abruski@gmail.com>
2024-04-12 16:50:01 +0300

    move files around and add exercises

---
+++ 06. Exercise - Classes and objects/08. Library/project/library.py
@@ -0,0 +1,23 @@
+from typing import List, Dict
+
+from user import User
+
+
+class Library:
+    def __init__(self):
+        self.user_records: List[User] = []
+        self.books_available: Dict[str: List[str]] = {}
+        self.rented_books: Dict[User: Dict[str:int]] = {}
+
+    def get_book(self, author: str, book_name: str, days_to_return: int, user: User):
+        if book_name in self.books_available[author]:
+            self.books_available[author].remove(book_name)
+            user.books.append(book_name)
+
+            if user.username in self.rented_books:
+                self.rented_books[user.username][book_name] = days_to_return
+            else:
+                self.rented_books[user.username] = {book_name: days_to_return}
+
+    def return_book(self, author: str, user: User, book_name: str):
+        pass
---
+++ 06. Exercise - Classes and objects/08. Library/project/registration.py
@@ -0,0 +1,13 @@
+from user import User
+from library import Library
+
+
+class Registration:
+    def add_user(self, user: User, library: Library):
+        pass
+
+    def remove_user(self, user: User, library: Library):
+        pass
+
+    def change_username(self, user_id: int, new_username: str, library: Library):
+        pass
